729fb0919bf1e1cac7070e6e674d33ddc469c544
Jamille Bittencourt <jamille.bittencourt@thoughtworks.com>
2021-07-22 20:48:22 -0300

    desmarreta a porta

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"eddy.com/todo/route"
 )
 
 func main() {
+	var PORT = ":9090"
 
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		PORT = ":" + envPort
+	}
 	// data.InitDatabase()
 
-	log.Print("Server on")
-	http.ListenAndServe(":9000", route.RegisterRoute())
+	log.Print("Server on ", PORT)
+	http.ListenAndServe(PORT, route.RegisterRoute())
 }
